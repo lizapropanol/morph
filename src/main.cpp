e4e7cf129b62e276b2f24a9f1cd929b358a65797
angemms <korabelnikov08@yandex.ru>
2026-05-14 21:45:06 +0300

    feat: embed Rubik fonts to ensure UI consistency across systems

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QFontDatabase>
 #include "core/Application.h"
 #include "utils/PathProvider.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    
+    QFontDatabase::addApplicationFont(":/assets/fonts/Rubik.ttf");
+    QFontDatabase::addApplicationFont(":/assets/fonts/Rubik-Italic.ttf");
     
     PathProvider::ensureConfigExists();
     
