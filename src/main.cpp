5548e6ff7a2f1249c23c93879e686ba8455a16f8
lizapropanol <korabelnikov08@yandex.ru>
2026-05-21 21:28:58 +0300

    fix(core): prevent multiple application instances using QLockFile

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <QApplication>
 #include <QFontDatabase>
+#include <QLockFile>
+#include <QDir>
 #include "core/Application.h"
 #include "utils/PathProvider.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    
+    QLockFile lockFile(QDir::tempPath() + "/morph.lock");
+    if (!lockFile.tryLock(100)) {
+        return 0;
+    }
     
     int fontId = QFontDatabase::addApplicationFont(":/assets/fonts/interblack.otf");
     if (fontId != -1) {
